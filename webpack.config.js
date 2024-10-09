a915b3534083dcc670d173f999dd4e235c972680
Anna Nabokova <talfril@gmail.com>
2024-10-09 20:51:20 +0200

    m

--- webpack.config.js
+++ webpack.config.js
@@ -84,8 +84,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
-    publicPath: '/'
-  },
+    publicPath: '/cookingList/',
+    },
   devServer: {
     static: path.join(__dirname, './dist'),
     compress: true,
