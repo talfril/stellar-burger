099fd20f83f7408d9df29f03c051ad290627f56f
Anna Nabokova <talfril@gmail.com>
2024-10-09 21:01:26 +0200

    m

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOMClient.createRoot(container!);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/stellar-burger/">
       <Provider store={store}>
         <App />
       </Provider>
